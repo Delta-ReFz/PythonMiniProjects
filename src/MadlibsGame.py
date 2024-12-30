cca2be6446212d5adb949efb8acc2f28d527325b
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2024-12-30 18:26:19 -0500

    finished the madlibs game

---
+++ src/MadlibsGame.py
@@ -0,0 +1,14 @@
+
+
+adjective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+adjective2 = input("Enter an adjective: ")
+verb1 = input("Enter a verb: ")
+adjective3 = input("Enter an adjective: ")
+
+
+
+print(f"Today i went to a {adjective1} Zoo")
+print(f"In an Exhibit, I saw a {noun1}")
+print(f"{noun1} eas {adjective2} and {verb1}")
+print(f"I was {adjective3}!")

8d82441458bc64911e3e97f346d704c2daa00da3
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2024-12-30 19:22:42 -0500

    Update weightConverter.py

---
+++ src/weightConverter.py
@@ -0,0 +1,15 @@
+
+weight = float(input("Enter you weight: "))
+unit = input("Kilograms or Pounds? (K or L): ").upper()
+
+if unit == "K":
+    weight = weight * 2.205
+    unit = "lbs"
+    print(f"Your weight is {weight:.2f} {unit}")
+elif unit == "L":
+    weight = weight / 2.205
+    unit = "kg"
+    print(f"Your weight is {weight:.2f} {unit}")
+else:
+    print(f"{unit} is not valid")
+

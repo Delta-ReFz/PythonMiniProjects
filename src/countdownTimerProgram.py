4bab44c04a889ed80de81aeb5154309bc3748ba1
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2025-01-04 02:12:14 -0500

    finished countdownTimeProgram

--- src/countdownTimerProgram.py
+++ src/countdownTimerProgram.py
@@ -8,7 +8,10 @@ myTime = int(input("Enter the time in seconds: "))
 
 for x in range(myTime,0, -1):
     seconds = x % 60
-    print(f"00:00:{seconds:02}")
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600) % 24
+    days = int(x / 86400)
+    print(f"{days:02}:{hours:02}:{minutes:02}:{seconds:02}")
     time.sleep(1)
 
 

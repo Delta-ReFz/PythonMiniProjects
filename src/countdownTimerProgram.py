093e764dec0f5f4efc62f56dcbf17ca01c5debd6
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2025-01-04 02:03:04 -0500

    Update countdownTimerProgram.py

--- src/countdownTimerProgram.py
+++ src/countdownTimerProgram.py
@@ -1,3 +1,16 @@
 '''
 for loops
 '''
+
+import time
+
+myTime = int(input("Enter the time in seconds: "))
+
+for x in range(myTime,0, -1):
+    seconds = x % 60
+    print(f"00:00:{seconds:02}")
+    time.sleep(1)
+
+
+
+print("TIME'S UP!")

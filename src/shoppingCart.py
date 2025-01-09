8d3baed984796b0858e60b142b1ffe845c2fe2c9
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2025-01-09 17:31:43 -0500

    Shopping cart program

---
+++ src/shoppingCart.py
@@ -0,0 +1,21 @@
+'''
+List, sets & tuples
+'''
+
+foods = []
+prices = []
+total = 0
+
+while True:
+    food = input("Enter a food to buy (q to quit): ").lower()
+    if food == "q":
+        break
+    else:
+        price = float(input(f"Enter the price of a {food}: $"))
+        foods.append(food)
+        prices.append(price)
+
+print("---------- YOUR CART ----------")
+
+for food in foods:
+    print(food, end=" ")
